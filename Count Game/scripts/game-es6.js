afd60bd2a82a7c7f59a6e08f15391abd877541fa
Veena <iyerveena08@gmail.com>
2018-07-02 09:32:10 -0400

    First Commit

---
+++ Count Game/scripts/game-es6.js
@@ -0,0 +1,27 @@
+// logic for Count game
+
+class Game{
+	constructor(){
+		console.log(`Welcome to the game. Version ${this.version()}`);
+
+		this.canvas = document.getElementById("game-canvas");
+		this.stage = new createjs.Stage(this.canvas);
+
+		createjs.Ticker.setFPS(60);
+
+		//keep re-drawing the stage
+		createjs.Ticker.on("tick", this.stage);
+
+		//testing code
+		var circle = new createjs.Shape();
+		circle.graphics.beginFill("red").drawCircle(0,0,40);
+		circle.x = circle.y = 100;
+		this.stage.addChild(circle);
+	}
+	version(){
+		return '1.0.0';	
+	}
+}
+
+// Start the game
+var game = new Game();
